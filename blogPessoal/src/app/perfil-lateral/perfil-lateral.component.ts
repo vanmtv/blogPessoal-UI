599987fcf2da6d863d57d20e1da14ceb541741b8
vanmtv <vanmtv@hotmail.com>
2020-12-03 11:56:18 -0300

    Add login and register

--- blogPessoal/src/app/perfil-lateral/perfil-lateral.component.ts
+++ blogPessoal/src/app/perfil-lateral/perfil-lateral.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil-lateral.component.css']
 })
 export class PerfilLateralComponent implements OnInit {
+  nome: string ='Vanessa Regina';
 
   constructor() { }
 
---
+++ blogPessoal/src/app/service/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  logar(userLogin: UserLogin){
+    return this.http.post('http://localhost:9000/usuarios/logar', userLogin);
+  }
+  
+  cadastrar(user: User){
+    return this.http.post('http://localhost:9000/usuarios/cadastrar', user);
+  }
+}
