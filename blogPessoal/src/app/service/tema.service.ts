5ced3bc60988574bd621902dd722501773ff4c7b
vanmtv <vanmtv@hotmail.com>
2020-12-08 15:24:29 -0300

    Add tema service

--- blogPessoal/src/app/service/tema.service.ts
+++ blogPessoal/src/app/service/tema.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Tema } from '../model/Tema';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TemaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', localStorage.getItem('token')!)
+  }
+
+  getAllTemas(): Observable<Tema[]>{
+    return this.http.get<Tema[]>('http://localhost:9000/tema', this.token)
+  }
+
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`http://localhost:9000/tema/${id}`, this.token)
+  }
+
+  postTema(tema: Tema){
+    return this.http.post<Tema>('http://localhost:9000/tema', tema, this.token)
+  }
 }
